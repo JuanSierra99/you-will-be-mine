ab3da968b3a01ce4d03b01b6eee83b75ca461e86
Juan Sierra <sierra.juan99@gmail.com>
2024-02-05 21:11:18 -0800

    overhaul confetti ! Cluster Bombs Extreme !!!
    Enhance Confetti Component Usability and Customization
    
    - Revamp the Confetti component to improve ease of use and flexibility.
    - Implement container size constraints, allowing better control with the 'overflow hidden' property.
    - Streamline cluster bomb mechanics, removing the need for explicit X and Y coordinate props.
    - Introduce additional props to offer extensive customization options for the confetti animation and appearance.
    
    These updates significantly enhance the component's adaptability to different use-cases and improve the developer experience by simplifying configuration and usage.

--- src/components/Confetti.svelte
+++ src/components/Confetti.svelte
@@ -6,16 +6,11 @@
 	export let displace: boolean;
 	export let vanish: boolean;
 	export let colors: Array<string> = [];
-	let cluster = false;
-	let clusterX: number;
-	let clusterY: number;
-
-	const halfScreen = screen.availHeight / 2;
-	const halfWidth = screen.availWidth / 2;
-
-	const randomSize = () => {
-		return `${Math.random() * 8 + 2}px`;
-	};
+	export let xDomain: [number, number];
+	export let yDomain: [number, number];
+	export let animationDuaration: [number, number];
+	export let showOverflow: boolean;
+	export let clusterBomb = false;
 
 	const getColor = () => {
 		if (colors.length > 0) {
@@ -29,96 +24,119 @@
 		return Math.random() * (maximum - minimum) + minimum;
 	};
 
+	// Initialize an array of n confetti pieces. Each piece has an id so that they can be individually removed if clicked.
 	let confettis: any;
 	confettis = Array(confettiAmmount)
 		.fill(null)
 		.map((_, i) => {
 			return {
 				id: i,
+				exploded: false,
 				style: `
-		--x-origin: ${xOrigin}px;
-		--y-origin: ${yOrigin}px;
-		--x-translate: ${randomBetween(-halfWidth, halfWidth)}px;
-		--y-translate: ${randomBetween(-halfScreen, halfScreen - 200)}px;
-		--animation-delay: ${randomBetween(0, 0.2)}s;
-		--animation-duration: ${randomBetween(1, 2)}s;
-		--rotation: ${randomBetween(-360, 360)}deg;
-		--animation-fill: ${displace ? 'none' : 'forwards'};
-		--dissapear: ${vanish ? '0' : '1'};
-
-    `
+				--x-origin: ${xOrigin}px;
+				--y-origin: ${yOrigin}px;
+				--x-translate: ${randomBetween(xDomain[0], xDomain[1])}px;
+				--y-translate: ${randomBetween(yDomain[0], yDomain[1])}px;
+				--animation-delay: ${randomBetween(0, 0.2)}s;
+				--animation-duration: ${randomBetween(animationDuaration[0], animationDuaration[1])}s;
+				--rotation: ${randomBetween(-360, 360)}deg;
+				--animation-fill: ${displace ? 'none' : 'forwards'};
+				--dissapear: ${vanish ? '0' : '1'};
+   				`
 			};
 		});
 
 	const clusterBombs = (id: number, event: MouseEvent) => {
-		clusterX = event.clientX;
-		clusterY = event.clientY;
-		cluster = true;
-		setTimeout(() => {
-			cluster = false;
-		}, 1000);
-		confettis = confettis.filter((c) => c.id !== id);
+		confettis = confettis.map((c) => {
+			if (c.id === id) {
+				return { ...c, exploded: true }; // Spread the confetti object and set exploded to true
+			}
+			return c; // Return the confetti as is for those that don't match the id
+		});
+		confettis = [...confettis];
+		removeConfetti(id);
+	};
+
+	const removeConfetti = (id: number) => {
+		// setTimeout(() => {
+		// 	confettis = confettis.filter((c) => c.id !== id);
+		// 	confettis = [...confettis];
+		// }, 4000);
 	};
 </script>
 
-{#each confettis as c}
-	<button style={c.style} class="confetti" on:click={(event) => clusterBombs(c.id, event)}>
-		<!-- <svg src="./heart-solid.svg" class="heart" alt="..." /> -->
-		<svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-			<!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-			<path
-				fill={getColor()}
-				d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 
-				268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"
-			/>
-		</svg>
-	</button>
-{/each}
-
-{#if cluster}
-	<Confetti
-		xOrigin={clusterX}
-		yOrigin={clusterY}
-		confettiAmmount={10}
-		vanish={true}
-		colors={[
-			'#c1121f',
-			'#eb5e28',
-			'#fcca46',
-			'#a1c181',
-			'#4361ee',
-			'#7209b7',
-			'#e4c1f9',
-			'#43aa8b',
-			'#2c6e49',
-			'#ff7b00',
-			'#bbd0ff'
-		]}
-	/>
-{/if}
+<div class="confetti-container" style="overflow:{showOverflow ? 'visible' : 'hidden'}">
+	{#each confettis as c}
+		<button style={c.style} class="confetti" on:click={(event) => clusterBombs(c.id, event)}>
+			{#if !c.exploded}
+				<svg class="heart-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+					<!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+					<path
+						fill={getColor()}
+						d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 
+			268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"
+					/>
+				</svg>
+			{:else if clusterBomb === true}
+				<Confetti
+					clusterBomb={true}
+					showOverflow={true}
+					animationDuaration={[1, 2]}
+					xDomain={[-100, 100]}
+					yDomain={[-100, 100]}
+					xOrigin={0}
+					yOrigin={0}
+					confettiAmmount={10}
+					vanish={false}
+					displace={false}
+					colors={[
+						'#c1121f',
+						'#eb5e28',
+						'#fcca46',
+						'#a1c181',
+						'#4361ee',
+						'#7209b7',
+						'#e4c1f9',
+						'#43aa8b',
+						'#2c6e49',
+						'#ff7b00',
+						'#bbd0ff'
+					]}
+				/>
+			{/if}
+		</button>
+	{/each}
+</div>
 
 <style>
-	.heart {
+	.confetti-container {
+		position: absolute;
+		pointer-events: none;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+	}
+
+	.heart-svg {
 		width: 10px;
 		height: auto;
 	}
 
 	.confetti {
-		top: var(--y-origin);
+		pointer-events: all;
 		left: var(--x-origin);
+		top: var(--y-origin);
 		position: absolute;
 		width: fit-content;
 		height: auto;
 		border: none;
 		background-color: transparent;
-		animation: explosion var(--animation-duration);
-		animation-delay: var(--animation-delay);
-		animation-fill-mode: var(--animation-fill);
+		animation: explosion var(--animation-duration) ease-out var(--animation-delay) 1 normal
+			var(--animation-fill);
 	}
 
 	@keyframes explosion {
-		from {
-		}
 		to {
 			transform: translate(var(--x-translate), var(--y-translate)) rotate(var(--rotation));
 			opacity: var(--dissapear);
